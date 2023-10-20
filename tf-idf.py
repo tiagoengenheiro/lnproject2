260e1ae457aeabdb8e39a74a94e18efc3dbb8eb7
tae <tiagoengenheiro.11@gmail.com>
2023-10-20 16:55:51 +0100

    add bert

--- tf-idf.py
+++ tf-idf.py
@@ -7,8 +7,11 @@ from sklearn.model_selection import train_test_split
 from collections import Counter
 import nltk
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+import nltk
 from utils import *
+
+
 nltk.download('punkt')
 df=pd.read_csv("train.txt",sep="\t",names=["LABEL","REVIEW"])
 print(df["LABEL"].value_counts()) #Balanced dataset
@@ -16,9 +19,7 @@ print(df.shape)
 df= df.loc[:, ::-1] #Reverse the order, so that LABEL is in the second column
 
 #Splitting
-X_train, X_test, y_train, y_test = train_test_split(np.array(df["REVIEW"]), np.array(df["LABEL"]), test_size=0.2, random_state=42)
-
-
+X_train, X_test, y_train, y_test = train_test_split(np.array(df["REVIEW"]), np.array(df["LABEL"]), test_size=0.2)
 
 
 X_train_preprocessed=pre_processing(X_train)
@@ -27,33 +28,6 @@ print(most_common_words)
 X_train_preprocessed=remove_common_words(X_train_preprocessed,most_common_words)
 analyse_feature_per_class(X_train_preprocessed,y_train)
 
-
-#Classification
-from sklearn.ensemble import RandomForestClassifier
-# vectorizer = TfidfVectorizer(ngram_range=(1,1),lowercase=False)
-# # #best_number_of_features(X_train,X_test,y_test)
-# #Get size feature before transformation
-# train_size_feature=get_size_feature(X_train)
-# #Tranform to obtain tf-idf count matrix
-# X_train_tf_idf=vectorizer.fit_transform(X_train)
-
-# #Combine with tf-idf features with size_feature for trainset
-# # X_train_tf_idf=np.array(X_train_tf_idf.todense())
-# # X_train_tf_idf=np.hstack((X_train_tf_idf,train_size_feature))
-
-# clf=LinearSVC(dual='auto', random_state=0, tol=1e-05,max_iter=1000,class_weight='balanced')
-
-# clf.fit(X_train_tf_idf, y_train)
-
-# test_size_feature=get_size_feature(X_test)
-# X_test_tf_idf=vectorizer.transform(X_test)
-# #Combine with tf-idf features with size_feature for test set
-# # X_test_tf_idf=np.array(X_test_tf_idf.todense())
-# # X_test_tf_idf=np.hstack((X_test_tf_idf,test_size_feature))
-
-# #Prediction
-# pred=clf.predict(X_test_tf_idf)
-# print(accuracy_score(y_test,pred))
 
 
 
--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ from collections import Counter
 import copy
 import re,string
 from nltk.tokenize import word_tokenize
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 def pre_processing(X_array):
     X_train_preprocessed=copy.deepcopy(X_array)
@@ -27,8 +29,8 @@ def get_most_common_words_among_label(X_array_preprocessed,y_array,k=300):
         examples=X_array_preprocessed[y_array==label]
         examples=np.concatenate(examples)
         word_counter=Counter(examples)
-        
         label_most_frequent_words=word_counter.most_common(k)
+        print(label_most_frequent_words)
         label_most_frequent_words=set(aux[0] for aux in label_most_frequent_words)
         if index==0:
             most_common_words=most_common_words.union(label_most_frequent_words)
@@ -62,6 +64,21 @@ def analyse_feature_per_class(X_array_pre_processed,y_array):
                     counter+=1
             label_number_of_uppers.append(counter/len(example))
         print(label,"Avg number uppers:",np.mean(label_number_of_uppers),"Std deviation of uppers:",np.std(label_number_of_uppers))
+
+
+#Classificatio
+def tf_idf_extractor(X_array,y_array):
+    vectorizer = TfidfVectorizer(ngram_range=(1,1),lowercase=False)
+    #train_size_feature=get_size_feature(X_array)
+    X_train_tf_idf=vectorizer.fit_transform(X_array)
+    clf=SVC(C=1.0,kernel='linear',class_weight='balanced',degree=1)
+    clf.fit(X_train_tf_idf, y_array)
+    #test_size_feature=get_size_feature(X_array)
+    X_test_tf_idf=vectorizer.transform(X_array)
+    pred=clf.predict(X_test_tf_idf)
+    print(accuracy_score(y_array,pred))
+
+
             
         
         
