af9ab9ded80971840ed0cc6a769fdbace09fc2c2
tae <tiagoengenheiro.11@gmail.com>
2023-10-24 23:08:20 +0100

    add confusion matrix

--- spliting_classes.py
+++ spliting_classes.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.metrics import accuracy_score, balanced_accuracy_score,confusion_matrix
 from sklearn.model_selection import train_test_split
 from collections import Counter
-import nltk
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from utils import *
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df=pd.read_csv("train.txt",sep="\t",names=["LABEL","REVIEW"])
 df= df.loc[:, ::-1]
@@ -61,6 +62,7 @@ for i,label in enumerate([class1,class2]):
     print(np.unique(label))
     vectorizer = TfidfVectorizer(ngram_range=(1,1),lowercase=False,max_features=best_hypers[i][1]["max_features"])
     X_train_tf_idf=vectorizer.fit_transform(X_train)
+    #clf=GaussianNB()
     clf=SVC(C=best_hypers[i][1]["C"],kernel=best_hypers[i][1]["kernel"],random_state=random_state)
     clf.fit(X_train_tf_idf, y_train)
     X_test_tf_idf=vectorizer.transform(X_test)
@@ -75,6 +77,21 @@ for el1,el2 in zip(pred1,pred2):
 
 #0.8107
 final_preds=np.array(final_preds)
-_, _, _, y_test = train_test_split(np.array(df["REVIEW"]),np.array(df["LABEL"]), test_size=0.2,random_state=random_state)
+_, X_test, _, y_test = train_test_split(np.array(df["REVIEW"]),np.array(df["LABEL"]), test_size=0.2,random_state=random_state)
+error_index=y_test!=final_preds
+df_test=pd.DataFrame({"Predicted":final_preds[error_index],"True":y_test[error_index],"Reviews":X_test[error_index]})
+df_test.to_csv("errors.tsv",sep="\t",index=False)
+
 print(accuracy_score(y_test,final_preds))
+labels=['DECEPTIVENEGATIVE','DECEPTIVEPOSITIVE','TRUTHFULNEGATIVE','TRUTHFULPOSITIVE']
+cm=confusion_matrix(y_test,final_preds,labels=labels)
+print(cm)
+plt.figure(figsize=(8, 6))
+num_classes=4
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["DCN","DCP","TFN","TFP"], yticklabels=["DCN","DCP","TFN","TFP"])
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.savefig('confusion_matrix.png')
+
 
